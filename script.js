4c8b2610f41ba8ba91d97d6c922eb4363c482a59
Prachi041997 <prachisown@gmail.com>
2020-11-27 12:36:21 +0530

    se

---
+++ script.js
@@ -0,0 +1,51 @@
+// var tl = gsap.timeline();
+
+// tl.from('#left', {
+//     width: 0,
+//     ease: Expo.easeInOut,
+//     duration: 2
+// })
+// .from('#right', {
+//     width: 0,
+//     ease: Expo.easeInOut,
+//     duration: 1
+// })
+// .from('#lbtmf', {
+//     height: 0,
+//     ease: Expo.easeInOut,
+//     duration: 2
+// }, '-=1.5')
+// .from('#lbtms', {
+//     height: 0,
+//     opacity: 0,
+//     ease: Expo.easeInOut,
+//     duration: 2
+// }, '-=1')
+// .from('.lbtmstag', {
+//     stagger: .2,
+//     y: 20,
+//     opacity: 0,
+//     ease: Expo.easeInOut,
+//     duration: 1.5
+// }, '-=2')
+// .from('.navstag', {
+//     stagger: .3,
+//     x: 20,
+//     opacity: 0,
+//     ease: Expo.easeInOut,
+//     duration: 2
+// }, '-=4')
+// .from('.detsstag', {
+//     stagger: .3,
+//     y: 30,
+//     opacity: 0,
+//     ease: Expo.easeInOut,
+//     duration: 2
+// }, '-=2')
+
+var prev = document.querySelector('#prev');
+var next = document.querySelector('#next');
+
+next.addEventListener('click', function(){
+    
+})
